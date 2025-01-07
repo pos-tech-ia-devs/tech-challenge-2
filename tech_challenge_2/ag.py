2c950e36e8e285b30e31c63b41cf0e71e87ef795
Antonio Fernandes <antoniomarcos997@gmail.com>
2025-01-06 21:48:44 -0300

    Update ag.py

--- tech_challenge_2/ag.py
+++ tech_challenge_2/ag.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import random
-
+import pandas as pd
+import numpy as np
 
 # Add the parent directory to the path so we can import the coins module
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -14,20 +15,41 @@ from coins import (
 )
 
 # Good indice to cripto is between 2% and 10%
-risk_free_rate = 0.06
+risk_free_rate = 0.04
 
+allowed_weights = [0.10, 0.20, 0.30, 0.40, 0.50]
+
+def generate_exact_weights(allowed_weights, num_coins):
+    while True:
+        weights = [random.choice(allowed_weights) for _ in range(num_coins)]
+        total = round(sum(weights), 2)
+        if total == 1.0:
+            return weights
 
 def generate_population(coins_quantity, population_size=10):
     possibles_coins = get_coins()
     population = []
     unique_individuals = set()
-
+    
     def generate_individual(possibles_coins, num_coins):
-        coins = tuple(random.sample(possibles_coins, num_coins))
-        weights = [random.random() for _ in range(num_coins)]
+        while True:
+            coins = set(random.sample(possibles_coins, num_coins))
+            # allowed_weights = [0.2, 0.4, 0.5, 0.3]
+            # weights = [random.choice(allowed_weights) for _ in range(num_coins)]
+            # weights = [random.random() for _ in range(num_coins)]
+
+            weights = generate_exact_weights(allowed_weights, num_coins)
+            exists_zero_weight = False
+            for w in weights:
+                if w == 0:
+                    exists_zero_weight = True
+            if len(coins) == len(weights) and not exists_zero_weight:
+                break
+
         sum_weights = sum(weights)
         normalized_weights = tuple(round(w / sum_weights, 2) for w in weights)
-        return coins, normalized_weights
+        normalized_coins = tuple(coins)
+        return normalized_coins, normalized_weights
 
     while len(population) < population_size:
         coins, weights = generate_individual(possibles_coins, coins_quantity)
@@ -39,6 +61,64 @@ def generate_population(coins_quantity, population_size=10):
             )
 
     return population
+
+
+def calculate_sharpe_ratio_for_multiple_cryptos(coins, weights, risk_free_rate=0.04):
+    """
+    Calcula o Índice de Sharpe para uma carteira composta por múltiplas criptomoedas.
+
+    :param paths: Lista de caminhos para os arquivos CSV de cada cripto, contendo as colunas 'Data' e 'Último'.
+    :param weights: Lista de pesos para cada cripto, somando 100%.
+    :param risk_free_rate: Taxa livre de risco anual.
+    :return: Índice de Sharpe, Retorno Anualizado, Volatilidade Anualizada.
+    """
+    all_returns = []
+    paths = []
+
+    for c in coins:
+        paths.append(f'quotations/{c}.csv')
+
+    # Iterar sobre cada caminho de arquivo
+    for path in paths:
+        # Carregar o arquivo CSV
+        data = pd.read_csv(path, delimiter=',', quotechar='\"')
+
+        # Converter a coluna 'Último' para valores numéricos
+        data['Último'] = data['Último'].str.replace('.', '').str.replace(',', '.').astype(float)
+
+        # Converter a coluna 'Data' para o formato de data
+        data['Data'] = pd.to_datetime(data['Data'], dayfirst=True)
+
+        data = data.sort_values("Data")
+
+        # Calcular os retornos diários
+        data['Retorno'] = data['Último'].pct_change()
+
+        # Excluir valores NA que possam resultar do cálculo de retorno
+        data.dropna(inplace=True)
+
+        all_returns.append(data[['Data', 'Retorno']])
+
+    # Juntar todos os dados de retorno por data
+    merged_data = all_returns[0]
+
+    for i in range(1, len(all_returns)):
+        merged_data = pd.merge(merged_data, all_returns[i], on='Data', suffixes=(f'_{i}', f'_{i+1}'))
+    print(merged_data)
+    # Calcular o retorno diário composto da carteira
+    merged_data['Retorno_Carteira'] = sum(weights[i] * merged_data[f'Retorno_{i+1}'] for i in range(1,len(weights)))
+    for i in range(len(weights)):
+        print(f"{i}")
+
+    # Calcular o retorno médio anualizado e desvio padrão da carteira
+    retorno_medio_diario = merged_data['Retorno_Carteira'].mean()
+    retorno_anualizado = (1 + retorno_medio_diario) ** 252 - 1
+    volatilidade_anualizada = merged_data['Retorno_Carteira'].std() * np.sqrt(252)
+
+    # Calcular o Índice de Sharpe
+    sharpe_ratio = (retorno_anualizado - risk_free_rate) / volatilidade_anualizada
+
+    return sharpe_ratio, retorno_anualizado, volatilidade_anualizada
 
 
 def calculate_fitness(population):
@@ -90,105 +170,6 @@ def selection_tournament(population, tournament_size=3):
         selected.append(winner)
     return selected
 
-
-# # Arithmetic Crossover
-# def crossover(parent1, parent2):
-#     def get_repeated_coins():
-#         repeated_coins = {"coins": [], "mean_weight": []}
-#         for index_base, coin_base in enumerate(parent1["coins"]):
-#             for index_compare, coin_compare in enumerate(parent2["coins"]):
-#                 if coin_base == coin_compare:
-#                     # can only have two repeats
-#                     mean_weights = (
-#                         parent1["weights"][index_base]
-#                         + parent2["weights"][index_compare]
-#                     ) / 2
-#                     repeated_coins["mean_weight"].append(mean_weights)
-#                     repeated_coins["coins"].append(coin_base)
-#                     break
-#
-#         return repeated_coins
-#
-#     repeated_coins = get_repeated_coins()
-#     coins = [*parent1["coins"], *parent2["coins"]]
-#     weights = [*parent1["weights"], *parent2["weights"]]
-#
-#     new_wallet = {"coins": [], "weights": []}
-#     for index_base, coin_base in enumerate(coins):
-#         if coin_base not in repeated_coins["coins"]:
-#             new_wallet["coins"].append(coin_base)
-#             new_wallet["weights"].append(weights[index_base])
-#
-#     coins = [*new_wallet["coins"], *repeated_coins["coins"]]
-#     weights = [*new_wallet["weights"], *repeated_coins["mean_weight"]]
-#
-#     def normalized_weights(weights):
-#         sum_weights = sum(weights)
-#         return [round(w / sum_weights, 2) for w in weights]
-#
-#     # Now all weights are normalized
-#     weights = normalized_weights(weights)
-#
-#     res =  {
-#         "coins": coins,
-#         "weights": weights,
-#         "fitness": None,
-#     }
-#     print(f"Crossover result: {res}")
-#     return res
-
-# Single point crossover
-def crossover(parent1, parent2):
-    # Ensure both parents have the same number of coins
-    if len(parent1["coins"]) != len(parent2["coins"]):
-        raise ValueError("Both parents must have the same number of coins.")
-
-    num_coins = len(parent1["coins"])
-
-    # Select a random crossover point
-    crossover_point = random.randint(1, num_coins - 1)
-
-    # Create new offspring by swapping coins and weights at the crossover point
-    child1_coins = parent1["coins"][:crossover_point] + parent2["coins"][crossover_point:]
-    child1_weights = parent1["weights"][:crossover_point] + parent2["weights"][crossover_point:]
-
-    # Ensure each coin is unique
-    unique_coins_weights = {}
-    for coin, weight in zip(child1_coins, child1_weights):
-        if coin not in unique_coins_weights:
-            unique_coins_weights[coin] = weight
-
-    child1_coins, child1_weights = zip(*unique_coins_weights.items())
-    # If the number of coins is less than the original, add random coins and weights from the parents
-    while len(child1_coins) < num_coins:
-        additional_index = random.randint(0, num_coins - 1)
-        additional_coin = parent1["coins"][additional_index]
-        additional_weight = parent1["weights"][additional_index]
-        if additional_coin not in child1_coins:
-            child1_coins += (additional_coin,)
-            child1_weights += (additional_weight,)
-
-    # If the number of coins is more than the original, trim the list
-    child1_coins = child1_coins[:num_coins]
-    child1_weights = child1_weights[:num_coins]
-
-    # Create new wallet
-    child1 = {
-        "coins": list(child1_coins),
-        "weights": list(child1_weights),
-        "fitness": None,
-    }
-
-    # Normalize weights to ensure the sum does not surpass 1
-    def normalize_weights(wallet):
-        total_weight = sum(wallet["weights"])
-        wallet["weights"] = [w / total_weight for w in wallet["weights"]]
-        return wallet
-
-    child1 = normalize_weights(child1)
-    # print(f"Crossover result: {child1}")
-    return child1
-
 # Two point crossover
 def crossover(parent1, parent2):
     # Ensure both parents have the same number of coins
@@ -224,12 +205,15 @@ def crossover(parent1, parent2):
     child1_coins = child1_coins[:num_coins]
     child1_weights = child1_weights[:num_coins]
 
+
     # Normalize weights to ensure the sum does not surpass 1
     def normalize_weights(weights):
         total_weight = sum(weights)
         return [w / total_weight for w in weights]
 
     child1_weights = normalize_weights(child1_weights)
+
+    child1_weights = generate_exact_weights(allowed_weights, num_coins)
 
     # Create new wallet
     child1 = {
@@ -246,25 +230,22 @@ def mutate(wallet):
     min_mutate_rate = random.randint(0, total_coins - 2)
     max_mutate_rate = random.randint(min_mutate_rate + 1, total_coins - 1)
 
-    # print(f"total_coins: {total_coins}")
-    # print(f"min_mutate_rate: {min_mutate_rate}")
-    # print(f"max_mutate_rate: {max_mutate_rate}")
-    # print(f"---------------------------------")
-
     def apply_mutate(arr, init, end):
         if init < 0 or end >= len(arr) or init > end:
-            raise ValueError("Intervalo inválido.")
+            raise ValueError("Invalid interval.")
 
         # Invert the interval
         arr[init : end + 1] = arr[init : end + 1][::-1]
         return arr
 
-    # print(f"Original Wallet: {wallet}")
     wallet["coins"] = apply_mutate(wallet["coins"], min_mutate_rate, max_mutate_rate)
-    wallet["weights"] = apply_mutate(
-        wallet["weights"], min_mutate_rate, max_mutate_rate
-    )
+    wallet["weights"] = apply_mutate(wallet["weights"], min_mutate_rate, max_mutate_rate)
 
-    # print(f"------------------------")
-    # print(f"Mutated Wallet: {wallet}")
+    # Ensure no weights are zero
+    wallet["weights"] = [w if w > 0 else random.uniform(0.01, 0.1) for w in wallet["weights"]]
+
+    # Normalize weights to ensure the sum does not surpass 1
+    total_weight = sum(wallet["weights"])
+    wallet["weights"] = [w / total_weight for w in wallet["weights"]]
+
     return wallet
